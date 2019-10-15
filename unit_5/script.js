adcf54f23f332af6bc4fc68be279f174f6404773
Boiko Iurii <boiko.iurii86@gmail.com>
2019-10-15 12:25:19 +0300

    finished unit_5

--- unit_5/script.js
+++ unit_5/script.js
@@ -152,20 +152,137 @@ document.querySelector('.out-10').innerText = out10;
 
 // task 11 --------------------
 
+let divs11 = document.querySelectorAll('.one');
+
+document.querySelector('.out-11').innerText = 'Количество div.one на странице: ' + divs11.length;
+
+
 // task 12 --------------------
+
+document.querySelector('.btn-12').onclick = () => {
+
+    let divs12 = document.querySelectorAll('.one');
+
+    for (let i = 0; i < divs12.length; i++) {
+
+        divs12[i].style.background = 'orange';
+
+    }
+
+}
 
 // task 13 --------------------
 
+document.querySelector('.btn-13').onclick = () => {
+
+    let divs13 = document.querySelectorAll('.one');
+
+    for (let i = 0; i < divs13.length; i++) {
+
+        console.log(divs13[i].innerHTML);
+
+    }
+
+}
+
 // task 14 --------------------
+
+document.querySelector('.btn-14').onclick = () => {
+
+    let inputs14 = document.querySelectorAll('input[type="text"]');
+
+    for (let i = 0; i < inputs14.length; i++) {
+
+        inputs14[i].setAttribute('placeholder', 'Введите данные');
+
+    }
+
+}
 
 // task 15 --------------------
 
+document.querySelector('.btn-15').onclick = () => {
+
+    let inputs15 = document.querySelectorAll('input[type="text"]');
+
+    console.log('Количество input[type="text"]: ', inputs15.length)
+
+}
+
 // task 16 --------------------
+
+let radioButtons16 = document.querySelectorAll('input[name="p1"]');
+
+document.querySelector('.btn-16').onclick = () => {
+
+    for (let i = 0; i < radioButtons16.length; i++) {
+
+        if (radioButtons16[i].checked) {
+
+            document.querySelector('.out-16').innerText = radioButtons16[i].value;
+
+        }
+
+    }
+
+}
 
 // task 17 --------------------
 
+document.querySelector('.btn-17').onclick = () => {
+
+    radioButtons16[0].checked = true;
+
+}
+
 // task 18 --------------------
+
+document.querySelector('.btn-18').onclick = () => {
+
+    for (let i = 0; i < radioButtons16.length; i++) {
+
+        radioButtons16[i].setAttribute('value', i);
+
+    }
+
+}
 
 // task 19 --------------------
 
+document.querySelector('.btn-19').onclick = () => {
+
+    let radioButtons19 = document.querySelectorAll('input[name="p2"]');
+
+    for (let i = 0; i < radioButtons19.length; i++) {
+
+        if (radioButtons19[i].checked) {
+
+            if (radioButtons19[i].value == '6') {
+
+                document.querySelector('.out-19').innerText = 'true';
+
+            } else {
+
+                document.querySelector('.out-19').innerText = 'false';
+
+            }
+
+        }
+
+    }
+
+}
+
 // task 20 --------------------
+
+let radioButtons20 = document.querySelectorAll('input[name="p3"]');
+
+for (let i = 0; i < radioButtons20.length; i++) {
+
+    radioButtons20[i].oninput = () => {
+
+        console.log('Был изменен input.');
+
+    }
+
+}
