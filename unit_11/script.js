ec2b5902669e95bce15fe4750073c4b8d3f9c504
Iurii <boiko.iurii86@gmail.com>
2019-10-25 00:08:30 +0300

    unit_11

--- unit_11/script.js
+++ unit_11/script.js
@@ -95,14 +95,8 @@ document.querySelector('.u-5__splice').onclick = () => {
 function spliceArr(arr, start, count) {
 
     if (start.length > 0 && count.length > 0) {
-
-        start = parseInt(start);
-        count = parseInt(count);
-
         arr.splice(start, count);
-
         return arr;
-
     }
     else if (start.length == 0 && count.length > 0) {
         return 'Введите значения индекса';
@@ -118,13 +112,113 @@ function spliceArr(arr, start, count) {
 
 // task 6 --------------------
 
+let arr6 = [];
+
+document.querySelector('.btn-6').onclick = () => {
+
+    let out6 = document.querySelector('.out-6');
+    let input6 = document.querySelector('.i-6');
+
+    out6.innerText = funcPush(arr6, input6);
+
+    input6.value = '';
+
+}
+
+function funcPush(arr, input) {
+
+    arr[arr.length] = input.value;
+
+    return arr;
+
+}
+
 // task 7 --------------------
+
+let arr7 = [1, 2, 3, 22, 555, 'as', 'w', 0, '#fff', 1];
+
+document.querySelector('.btn-7').onclick = () => {
+
+    let out7 = document.querySelector('.out-7');
+
+    out7.innerText = funcPop(arr7);
+
+}
+
+function funcPop(arr) {
+
+    if (arr.length > 0)
+        arr.length = arr.length - 1;
+
+    return arr;
+
+}
 
 // task 8 --------------------
 
+document.querySelector('.btn-8').onclick = () => {
+
+    let out8 = document.querySelector('.out-8');
+
+    out8.innerText = funcShift(arr7);
+
+}
+
+function funcShift(arr) {
+
+    for (let i = 1; i <= arr.length; i++) {
+        arr[i - 1] = arr[i];
+    }
+
+    if (arr.length > 0)
+        arr.length = arr.length - 1;
+
+    return arr;
+
+}
+
 // task 9 --------------------
 
+let test = [1, 2, 3, 4, 5]
+
+document.querySelector('.btn-9').onclick = () => {
+    let input9 = document.querySelector('.i-9')
+    let out9 = document.querySelector('.out-9');
+
+    out9.innerText = funcUnShift(test, input9);
+
+}
+
+function funcUnShift(arr, input) {
+
+    let b = [];
+
+    console.log(arr.length);
+
+    b.length = arr.length + 1;
+
+    console.log(arr.length);
+
+    for (let i = 0; i < arr.length; i++) {
+
+        b[i + 1] = arr[i];
+        // b[i] = input.value;
+    }
+
+    return b;
+
+}
+
 // task 10 --------------------
+
+let arr10 = [2, 4, 6, 8, 10, 'hello'];
+
+document.querySelector('.u-10__reverse').onclick = () => {
+
+    arr10.reverse();
+    document.querySelector('.out-10').innerHTML = arr10;
+
+}
 
 // task 11 --------------------
 
