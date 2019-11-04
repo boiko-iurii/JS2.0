2d04f8c29597c00ad4fd42e2fe80fb56de376e5a
Boiko Iurii <boiko.iurii86@gmail.com>
2019-11-04 18:29:45 +0200

    unit_14

--- unit_14/script.js
+++ unit_14/script.js
@@ -1,12 +1,20 @@
-fetch('http://api.openweathermap.org/data/2.5/weather?q=Poltava,ua&APPID=58d6fdf40efaefc74dc8e0da3793eddc')
-    .then(function (resp) { return resp.json() })
-    .then(function (data) {
-        console.log(data);
-        document.querySelector('.city').innerHTML = data.city.name;
-        document.querySelector('.country').innerHTML = data.city.country;
-        document.querySelector('.cloudy').innerHTML = data.weather[0].main;
+document.querySelector('.btn').onclick = () => {
 
-    })
-    .catch(function () {
+    let citiesId = document.querySelector('.cities-list').value;
 
-    })
+    fetch(`http://api.openweathermap.org/data/2.5/weather?id=${citiesId}&APPID=58d6fdf40efaefc74dc8e0da3793eddc`)
+        .then(function (resp) { return resp.json() })
+        .then(function (data) {
+
+            console.log(data);
+
+            document.querySelector('.city').innerHTML = data.name;
+            document.querySelector('.country').innerHTML = data.sys.country;
+            document.querySelector('.cloudy').innerHTML = data.weather[0].description;
+
+
+        })
+        .catch(function () {
+
+        })
+}
