8615fdaeacbb73ad36d8a842385c79d6f673596d
Boiko Iurii <boiko.iurii86@gmail.com>
2019-11-05 14:15:16 +0200

    finished unit_14

--- unit_14/script.js
+++ unit_14/script.js
@@ -7,19 +7,23 @@ document.querySelector('.btn').onclick = () => {
         .then(function (data) {
 
             console.log(data);
+            console.log(data.sys.sunrise);
+            console.log(data.sys.sunset);
 
             document.querySelector('.city').textContent = data.name;
             document.querySelector('.country').textContent = data.sys.country;
             document.querySelector('.middle-content__img img').setAttribute('src', `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`);
             document.querySelector('.middle-content__temperature').innerHTML = getTemperature(data.main['temp']);
-            document.querySelector('.middle-content__hi span').innerHTML = getTemperature(data.main['temp_min']);
-            document.querySelector('.middle-content__lo span').innerHTML = getTemperature(data.main['temp_max']);
+            document.querySelector('.middle-content__hi span').innerHTML = getTemperature(data.main['temp_max']);
+            document.querySelector('.middle-content__lo span').innerHTML = getTemperature(data.main['temp_min']);
             document.querySelector('.middle-content__weather-descr').textContent = data.weather[0].description;
-            document.querySelector('.humidity span').textContent = `${data.main['humidity']}%`;
 
-            document.querySelector('.sunrise span').textContent = getTime(data.sys.sunrise);
-            document.querySelector('.sunset span').textContent = getTime(data.sys.sunset);
-            document.querySelector('.wind span').textContent = data.wind.speed;
+            document.querySelector('.rain span').textContent = getRain(data.rain['3h']);
+            document.querySelector('.humidity span').textContent = `${data.main['humidity']}%`;
+            document.querySelector('.wind span').innerHTML = getWind(data.wind.speed, data.wind.deg);
+            document.querySelector('.sunrise span').textContent = getTime(data.sys.sunrise, data.timezone);
+            document.querySelector('.sunset span').textContent = getTime(data.sys.sunset, data.timezone);
+            document.querySelector('.pressure span').textContent = `${data.main.pressure} hPa`;
 
 
         })
@@ -33,20 +37,64 @@ function getTemperature(temp) {
     temp = Math.floor(temp - 273);
 
     if (temp >= 0)
-        temp = `+${temp}&deg;`;
+        return `+${temp}&deg;`;
     else
-        temp = `${temp}&deg;`;
-
-    return temp;
+        return `${temp}&deg;`;
 
 }
 
-function getTime(time) {
+function getRain(rain) {
+    if (rain)
+        return `${rain} mm`;
+    else
+        return `--`;
+}
 
-    date = new Date(time);
+function getTime(time, timeZone) {
 
-    date.toString;
+    let minutes = Math.floor((time % 3600) / 60);
+    let houres = Math.floor((time % 86400) / 3600) + timeZone / 3600;
 
-    return date;
+    if (houres >= 12)
+        return `${houres - 12}:${minutes} PM`;
+    else
+        return `${houres}:${minutes} AM`;
+
+}
+
+function getWind(speed, direction) {
+
+    let deg = direction;
+
+    switch (true) {
+        case (direction <= 22.5 || direction > 337.5):
+            direction = 'North'
+            break;
+        case (direction <= 67.5):
+            direction = 'North-East'
+            break;
+        case (direction <= 112.5):
+            direction = 'East'
+            break;
+        case (direction <= 157.5):
+            direction = 'South-East'
+            break;
+        case (direction <= 202.5):
+            direction = 'South'
+            break;
+        case (direction <= 247.5):
+            direction = 'South-West'
+            break;
+        case (direction <= 292.5):
+            direction = 'West'
+            break;
+        case (direction <= 337.5):
+            direction = 'North-West'
+            break;
+    }
+
+    speed += ' m/s';
+
+    return `${speed}, ${direction} (${deg}&deg;)`;
 
 }
