fdbdddbdb5646b6bbd52978daf745866d2cc6b8d
Iurii <boiko.iurii86@gmail.com>
2019-12-15 23:38:37 +0200

    Unit_28

---
+++ unit_28/unit_28.js
@@ -0,0 +1,82 @@
+// Task 1
+// Создайте класс Goods. Класс должен содержать свойства name, amount. Создайте на его основе объект goods. Выведите в консоль созданный объект. Помните, все классы - в отдельных файлах. Имена классов с большой буквы.
+
+class Goods {
+    constructor(name = 'name', amount = 15) {
+        this.name = name;
+        this.amount = amount;
+    }
+}
+
+//Task 2.
+//  Добавьте в класс Goods свойства image и count - картинка и количество на складе.
+
+class Goods2 extends Goods {
+    constructor(name = 'dd', amount, image = 'img', count = 'abc') {
+        super(name, amount);
+        this.image = image;
+        this.count = count;
+    }
+}
+
+//Task 3. 
+//  Добавьте в класс Goods метод draw, который будет выводить div с изображением, названием товара, ценой - в указанный элемент (дозапись).
+
+class Goods3 extends Goods2 {
+    constructor(div = 'div') {
+        super();
+        this.div = div;
+    }
+    // draw(element) {
+    //     let div = document.createElement('div');
+    //     div.classList.add('product');
+    //     div.innerHTML = 'image' + ' name';
+    //     element.append(div)
+    // }
+    consoleLog() {
+        console.log(this);
+    }
+
+}
+
+let goods = new Goods3();
+
+goods.consoleLog();
+
+
+// Task 4.
+// Создайте на основе класса Goods объект goods2, заполните свойства, примените метод draw для вывода товара на страницу в блок out-4.
+
+
+
+//Task 5.
+// Создайте класс Goods2, который наследуется от Goods. Добавьте ему свойство sale равное true или false. Перезапишите метод draw так, чтобы он выводил информацию о распродажах.
+
+
+// Task 6.
+// Создайте на основе класса Goods2 объект goods3. Заполните все поля. Выведите товар на страницу с помощью метода draw. Вывод осуществить в out-6.
+
+
+// Task 7.
+// Создайте класс Valid, который содержит свойства email, password, isValid. И метод validate. Метод validate должен проверять длину пароля и писать false в isValid если длина меньше 6 и true если больше. Изначально свойство isValid равно false.
+
+
+//Task 8.
+// Создайте объект на основе класса Valid и задайте ему имя и пароль длиной 5 символов. Запустите метод validate() и выведите в консоль свойство isValid.
+
+
+//Task 9.
+// Создайте объект на основе класса Valid и задайте ему имя и пароль длиной 7 символов. Запустите метод validate() и выведите в консоль свойство isValid.
+
+
+//Task 10.
+// Унаследуйтесь от класса Valid и создайте класс Valid2. Расширьте его свойствами emaiError, passwordError. По умолчанию, они равны пустой строке. Перезапишите метод validate(), помимо проверки пароля, он должен содержать еще проверку свойства email на пустоту. Если поле email пустое - то isValid - false. Также, в случае ошибки валидации в поле emailError пишется сообщение ‘email empty’, в поле passwordError - ‘min length 6’.
+
+
+//Task 11.
+// Создайте на основе класса Valid2 объект valid2 и задайте пустой емейл и длину пароля больше 7. Запустите метод validate(). Выведите объект в консоль.
+
+
+//Task 12.
+// Создайте на основе класса Valid2 объект valid3 и задайте не пустой емейл и длину пароля больше 7. Запустите метод validate(). Выведите объект в консоль.
+
